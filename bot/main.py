95bd87ba81aa638201b2c50f360783e3ca8571ce
Mmdsiner <mmdkingoff@gmail.com>
2026-04-27 19:45:22 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,38 +1,30 @@
+import sys
+import os
 import asyncio
-import logging
+
+# حل مشکل پیدا نکردن ماژول bot
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 
 from config import BOT_TOKEN
-from database import connect, init_db
 
-from handlers import user, payment, admin
-
+# ایمپورت هندلرها
+from handlers import user, admin, payment
 
 async def main():
-    logging.basicConfig(level=logging.INFO)
-
     bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
     dp = Dispatcher()
 
-    # اتصال دیتابیس
-    await connect()
-    await init_db()
-
-    # حل مشکل webhook و conflict
-    await bot.delete_webhook(drop_pending_updates=True)
-
     # ثبت هندلرها
     dp.include_router(user.router)
-    dp.include_router(payment.router)
     dp.include_router(admin.router)
+    dp.include_router(payment.router)
 
-    print("BOT STARTED ✅")
+    print("Bot is running...")
 
-    # اجرای ربات
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
