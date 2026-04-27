5472314daf384470bd9a679d95ff662af8039ad0
Mmdsiner <mmdkingoff@gmail.com>
2026-04-27 18:37:04 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,3 @@
-import asyncio
-from aiogram import Bot, Dispatcher
-
-from config import BOT_TOKEN
-from database import connect, init_db
-
-from handlers import user, admin, payment
-
 async def main():
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
@@ -13,11 +5,10 @@ async def main():
     await connect()
     await init_db()
 
+    await bot.delete_webhook(drop_pending_updates=True)
+
     dp.include_router(user.router)
     dp.include_router(payment.router)
     dp.include_router(admin.router)
 
     await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
